b8966dd19c6f790cad125371658eafa1dd22631d
alanpoon <brightshadowx@gmail.com>
2019-07-02 09:46:34 +0800

    stablize

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,8 @@ extern crate stdweb;
 #[cfg(feature = "now")]
 extern crate time;
 
+#[cfg(any(feature = "now", feature = "wasm-bindgen"))]
+extern crate web_sys;
 
 #[cfg(any(
 not(any(target_arch = "wasm32", target_arch = "asmjs")),
